83bde370e945c3b6e9562f686068c37143abf4c2
Eugene <1985edw@gmail.com>
2021-06-19 09:01:21 +0200

    Removed password

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-imports: [MongooseModule.forRoot('mongodb+srv://dbuser:P@ssword01!@stgnavalbase.tonvo.mongodb.net/pantry?retryWrites=true&w=majority'), ItemsModule],
+imports: [MongooseModule.forRoot('mongodb+srv://dbuser:@stgnavalbase.tonvo.mongodb.net/pantry?retryWrites=true&w=majority'), ItemsModule],
 controllers: [AppController],
 providers: [AppService],
 })
